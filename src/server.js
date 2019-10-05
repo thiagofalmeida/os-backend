a17fba4acf375097bd32976b59a6b9c3b8e2ab61
Thiago F. Almeida <thiagofalmd@gmail.com>
2019-10-05 18:11:36 -0300

    Route, controller and model

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://tfalmd:tfalmd@omnistack-f5hfd.mongodb.net/omni?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 // req.query = filtros (get)
 // req.params = route params (put e delete)
 // req.body = corpo da req (post e put)
-
 app.use(express.json());
 app.use(routes);
 
