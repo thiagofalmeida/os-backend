44013bd1ef703706cac6c9866b6e8594178fc555
Thiago F. Almeida <thiagofalmd@gmail.com>
2019-10-08 20:17:54 -0300

    Return the thumbnails to frontend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 require('dotenv/config');
@@ -15,7 +17,9 @@ mongoose.connect(process.env.MONGO_LINK, {
 // req.params = route params (put e delete)
 // req.body = corpo da req (post e put)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '../', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
